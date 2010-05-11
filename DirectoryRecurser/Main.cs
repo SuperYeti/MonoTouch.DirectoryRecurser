bbea252cb31a2a9307689b438185906b2a68b1e2
Warren Moxley <wmoxley@yvr001osx.it1services.local>
2010-05-10 19:46:14 -0700

    Initial code load of a simple file system browser for the iphone with MonoTouch.Dialog.  It still needs to be cleaned up and have images etc added but good start.

---
+++ DirectoryRecurser/Main.cs
@@ -0,0 +1,84 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using System.IO;
+
+namespace DirectoryRecurser
+{
+	public class Application
+	{
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args);
+		}
+	}
+
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+			
+			window.MakeKeyAndVisible ();
+			
+			window.AddSubview (navigation.View);
+			
+			ShowDirectoryTree("/Users/wmoxley/Documents/", false);
+			
+			return true;
+		}
+		
+		private void ShowDirectoryTree(string basePath, bool pushing)
+		{
+			RootElement root = new RootElement(basePath)
+			{
+				BuildListing(basePath)
+			};
+			
+			var dv = new DialogViewController (root, pushing);
+			
+			navigation.PushViewController (dv, true);
+			
+		}
+		
+		private Section BuildListing(string basePath)
+		{
+			Section sect = new Section();
+			
+			foreach(string dir in Directory.GetDirectories(basePath))
+			{
+				MultilineElement element = new MultilineElement (dir);
+				element.Tapped += delegate { ShowDirectoryTree(dir, true); };
+			
+				sect.Add(element);
+			}
+			
+			foreach(string fil in Directory.GetFiles(basePath))
+			{
+				MultilineElement element = new MultilineElement (basePath);
+				element.Tapped += delegate { Utilities.UnsuccessfulMessage("File: " + fil + " tapped"); };
+			
+				sect.Add(element);
+				
+			}
+			
+			return sect;
+			
+		}
+
+		// This method is required in iPhoneOS 3.0
+		public override void OnActivated (UIApplication application)
+		{
+			
+		}
+		
+	}
+	
+}
